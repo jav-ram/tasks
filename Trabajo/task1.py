d410719d00560070e9c7b3d799377b252794aacf
javoz <javoz@localhost.localdomain>
2018-09-10 12:08:49 -0500

    nueva implementacion

--- Trabajo/task1.py
+++ Trabajo/task1.py
@@ -1,77 +1,17 @@
-# extract tree
-
-# permutation lib
-from itertools import product
-
-# get permutations of choices
-
 tree = []
-
-size = 100
-secuencia = []
-
+# read file
 txtTree = open("piramide.txt", "r")
-
+# make it an array
 for lines in txtTree:
     lines = lines.rstrip()  
     characters = lines.split(" ")
     tree.append(characters)
 
-
-
-ongoing = True
-
-lastIteration = []
-ids = []
-
-where = 0
-
-
-for j in range(size-1, -1, -1):
-    # search for the biggest one 
-    id = 0
-
-    if (j == size - 1):
-        for i in range(0, size):
-            if (tree[j][i] > tree[j][id]):
-                #print tree[j][id]
-                id = i
-    else:
-        
-        if (ids[-1] == 0):
-            if (j == 0):
-                id = 0
-            elif (tree[j][ids[-1]] < tree[j][ids[-1] + 1]):
-                id = ids[-1]
-            else:
-                id = ids[-1] + 1
-        elif (ids[-1] == len(tree[j])):
-            if (tree[j][ids[-1] - 1] > tree[j][ids[-1] - 2]):
-                id = ids[-1] - 1
-            else:
-                id = ids[-1] - 2
-
+for y in range((len(tree) - 2), -1, -1):
+    for x in range(0, len(tree[y])):
+        if int(tree[y + 1][x]) > int(tree[y + 1][x + 1]):
+            tree[y][x] = int(tree[y][x]) + int(tree[y + 1][x])
         else:
-            print str(j) + "" +  str(ids[-1])
-            
-            if (tree[j][ids[-1]] > tree[j][ids[-1] - 1]):
-                id = ids[-1]
-            else:
-                id = ids[-1] - 1
+            tree[y][x] = int(tree[y][x]) + int(tree[y + 1][x + 1])
 
-    print ids
-    
-    ids.append(int(id))
-
-
-    
-print("Path de atras para adelante (ids o posicion)" + str(ids))
-resultado = 0
-index = 0
-
-p = size
-for y in range(0, size):
-    print tree[size - 1 -y][ids[y]]
-    resultado = resultado + int(tree[size - 1 -y][ids[y]])
-
-print("Base es de:" + str(resultado))
+print "La base de la piramide es de: " + str(tree[0][0])
